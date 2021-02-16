023b8a52f1c9dbec81b49c24b1b464a75996d1b7
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-16 12:06:33 +0000

    for heroku

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
-    flash[:success] = 'micropostをお気に入りに登録しました。'
+    #flash[:success] = 'micropostをお気に入りに登録しました。'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
-    flash[:success] = 'お気に入りを解除しました。'
+    #flash[:success] = 'お気に入りを解除しました。'
     redirect_back(fallback_location: root_path)
     #redirect_to root_path
   end
